1689ef4a7f92a34cfb40f76baf8c1fb48fb55c71
ykhamlichi <ykhamlichi@users.noreply.github.com>
2019-01-22 13:12:30 +0100

    Add files via upload

---
+++ src/main/java/ma/ensaf/demo/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,14 @@
+package ma.ensaf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ma/ensaf/demo/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,17 @@
+package ma.ensaf.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
